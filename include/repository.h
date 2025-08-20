4ed5b46a93ea088f786df7a89d4d3707ec2a6110
luczod <kzod5611@gmail.com>
2025-08-20 17:33:33 -0300

    feat: creaate interfaces

---
+++ include/repository.h
@@ -0,0 +1,19 @@
+#ifndef REPOSITORY_H
+#define REPOSITORY_H
+
+#include <stdbool.h>
+#include "person.h"
+
+typedef struct
+{
+    void *object;
+    void (*store)(void *object, const PERSON_T person);
+    void (*store_list)(void *object, const PERSON_T *person_list, int items_amount);
+    void (*recovery_list)(void *object, const PERSON_T **person_list, int *items_amount);
+} REPOSITORY_BASE;
+
+bool person_store(REPOSITORY_BASE *repository, const PERSON_T *person);
+bool person_store_list(REPOSITORY_BASE *repository, const PERSON_T *person_list, int items_amount);
+bool person_recovery_list(REPOSITORY_BASE *repository, const PERSON_T **person_list, int *item_amount);
+
+#endif /* REPOSITORY_H */
